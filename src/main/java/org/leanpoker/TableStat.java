682b2242e24220ef91085ed2c3383414267f5d76
oliver.vrabel <oliver.vrabel@gmail.com>
2019-02-09 14:20:51 +0100

    bet 0

--- src/main/java/org/leanpoker/TableStat.java
+++ src/main/java/org/leanpoker/TableStat.java
@@ -1,14 +1,45 @@
 package org.leanpoker;
 
-import org.leanpoker.player.Player;
+import com.google.gson.JsonElement;
 
 public class TableStat {
 
     private String name;
+    private JsonElement hand;
+    private String cardsOnTable;
+    private JsonElement bet;
 
-    public static void main(String[] args) {
-        Player player = new Player();
-
-
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public JsonElement getHand() {
+        return hand;
+    }
+
+    public void setHand(JsonElement hand) {
+        this.hand = hand;
+    }
+
+    public String getCardsOnTable() {
+        return cardsOnTable;
+    }
+
+    public void setCardsOnTable(String cardsOnTable) {
+        this.cardsOnTable = cardsOnTable;
+    }
+
+    public JsonElement getBet() {
+        return bet;
+    }
+
+    public void setBet(JsonElement bet) {
+        this.bet = bet;
+    }
+
+
 }
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.leanpoker.TableStat;
 
 import java.util.Map;
 
@@ -12,21 +13,28 @@ public class Player {
     static final String VERSION = "DumbRobot1.1";
 
     public static int betRequest(JsonElement request) {
-        int bet = 0;
-        JsonObject jobject = request.getAsJsonObject();
-        jobject = jobject.getAsJsonObject("game_state");
-        JsonArray jarray = jobject.getAsJsonArray("hole_cards");
-        jobject = jarray.get(0).getAsJsonObject();
-        String result = jobject.get("suit").getAsString();
+
+        TableStat tableStat = new TableStat();
+
+        JsonObject jObject = request.getAsJsonObject();
+
+        JsonElement cardsUsed = jObject.get("cards_used");
+        JsonElement bet = jObject.get("bet");
+
+        tableStat.setHand(cardsUsed);
+        tableStat.setBet(bet);
+
+        JsonArray jarray = jObject.getAsJsonArray("hole_cards");
+        jObject = jarray.get(0).getAsJsonObject();
+
+        String result = jObject.get("suit").getAsString();
 
 
-
-
-        if(result=="hearts"){
-            bet = 10;
-        }return 0;
+        return 0;
     }
 
     public static void showdown(JsonElement game) {
+
+
     }
 }
