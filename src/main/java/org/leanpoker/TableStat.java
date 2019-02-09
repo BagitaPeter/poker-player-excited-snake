d41602bfa6b033c021e57b162111d90db0f2c552
oliver.vrabel <oliver.vrabel@gmail.com>
2019-02-09 13:16:14 +0100

    dumb 1.1

---
+++ src/main/java/org/leanpoker/TableStat.java
@@ -0,0 +1,14 @@
+package org.leanpoker;
+
+import org.leanpoker.player.Player;
+
+public class TableStat {
+
+    private String name;
+
+    public static void main(String[] args) {
+        Player player = new Player();
+
+
+    }
+}
--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,17 +1,19 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "PlayerName";
+    static final String VERSION = "DumbRobot1.1";
 
     public static int betRequest(JsonElement request) {
         int bet = 0;
-
-        JsonObject  jobject = request.getAsJsonObject();
+        JsonObject jobject = request.getAsJsonObject();
         jobject = jobject.getAsJsonObject("game_state");
         JsonArray jarray = jobject.getAsJsonArray("hole_cards");
         jobject = jarray.get(0).getAsJsonObject();
@@ -19,11 +21,10 @@ public class Player {
 
 
         if(result=="hearts"){
-            bet = 11;
+            bet = 10;
         }return bet;
     }
 
     public static void showdown(JsonElement game) {
-
     }
 }
