5ec323f588310f63fe0f08e144367ac42947ad48
Gergő Vetési <vetesigergo@gmail.com>
2019-02-09 14:52:07 +0100

    .

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,25 +6,11 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "testplayer2";
+    static final String VERSION = "testplayer4";
 
     public static int betRequest(JsonElement request) {
-        int bet = 0;
 
-        JsonObject  jobject = request.getAsJsonObject();
-        jobject1 = jobject.getAsJsonObject("game_state");
-        JsonArray jarray = jobject1.getAsJsonArray("players");
-        jobject2 = jarray.get(1).getAsJsonObject();
-
-        JsonArray jarray1 = jobject2.getAsJsonArray("hole_cards");
-        jobject3 = jarray1.get(0).getAsJsonObject();
-
-        String result = jobject3;
-
-
-        if(result=="hearts"){
-            bet = 23;
-        }return bet;
+        return 20;
     }
 
     public static void showdown(JsonElement game) {
